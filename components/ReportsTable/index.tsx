5b846c1c1084ac402fa4f3f1c018f6395841a3e3
Arian Cayton <ariancayton@gmail.com>
2022-05-09 02:19:48 +0800

    add: table with user stats

---
+++ components/ReportsTable/index.tsx
@@ -0,0 +1,79 @@
+import UserReports, { UserReportsProps } from "../UserReports";
+import styles from "./ReportsTable.module.css";
+
+// import { FC } from 'react'
+const ReportsTable = () => {
+  const userReport1 = {
+    userID: 1,
+    username: "hotdog",
+    email: "hotdog@mail.com",
+    photo: "",
+    totalPosts: 69,
+    reports: 13,
+    hatePercent: 0.44,
+    notPercent: 0.22,
+    prfnPercent: 0.33145,
+    racePercent: 0.6341,
+    religionPercent: 0.323,
+    genderPercent: 0.1353,
+  } as UserReportsProps;
+
+  const userReport2 = {
+    userID: 2,
+    username: "nice",
+    email: "nice@mail.com",
+    photo: "",
+    totalPosts: 6,
+    reports: 133,
+    hatePercent: 0.04,
+    notPercent: 0.02,
+    prfnPercent: 0.0335,
+    racePercent: 0.06,
+    religionPercent: 0.323,
+    genderPercent: 0.03,
+  } as UserReportsProps;
+
+  const userReportsList = [userReport1, userReport2];
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.headerContainer}>
+        <div className={styles.userInfoHeader}>
+          <p>User Information</p>
+        </div>
+        <div className={styles.statsHeader}>
+          <p>Reports</p>
+          <p>HATE</p>
+          <p>NOT</p>
+          <p>PRFN</p>
+          <p>RACE</p>
+          <p>REL</p>
+          <p>GEN</p>
+          <p></p>
+        </div>
+      </div>
+      <div className={styles.allStatsContainer}>
+        {userReportsList.map((userReports) => (
+          <UserReports
+            key={userReports.userID}
+            userID={userReports.userID}
+            username={userReports.username}
+            email={userReports.email}
+            photo={userReports.photo}
+            totalPosts={userReports.totalPosts}
+            reports={userReports.reports}
+            hatePercent={userReports.hatePercent}
+            notPercent={userReports.notPercent}
+            prfnPercent={userReports.prfnPercent}
+            racePercent={userReports.racePercent}
+            religionPercent={userReports.religionPercent}
+            genderPercent={userReports.genderPercent}
+          />
+        ))}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ReportsTable;
---
+++ components/UserReports/index.tsx
@@ -0,0 +1,80 @@
+import { FC } from "react";
+import Image from "next/image";
+import { Button } from "@mui/material";
+import styles from "./UserReports.module.css";
+
+export interface UserReportsProps {
+  userID: number;
+  username: string;
+  email: string;
+  photo?: string;
+  totalPosts: number;
+  reports: number;
+  hatePercent: number;
+  notPercent: number;
+  prfnPercent: number;
+  racePercent: number;
+  religionPercent: number;
+  genderPercent: number;
+}
+
+const UserReports: FC<UserReportsProps> = ({
+  // userID,
+  username,
+  email,
+  photo,
+  totalPosts,
+  reports,
+  hatePercent,
+  notPercent,
+  prfnPercent,
+  racePercent,
+  religionPercent,
+  genderPercent,
+}: UserReportsProps) => {
+  return (
+    // Main container
+    <div className={styles.mainContainer}>
+      {/*Image and User Info Container */}
+      <div className={styles.imageAndUserInfoContainer}>
+        {/*Image container*/}
+        <div className={styles.imageContainer}>
+          {!photo || photo === "" ? (
+            <div className="rounded-full w-[54px] h-[54px] bg-[#B1B1B1]" />
+          ) : (
+            <Image
+              className="rounded-full"
+              src={photo}
+              width={54}
+              height={54}
+              layout={"fixed"}
+              priority
+            />
+          )}
+        </div>
+        {/*User info container*/}
+        <div className={styles.userInfoContainer}>
+          <p className={styles.username}>{username}</p>
+          <p className={styles.email}>{email}</p>
+          <p className={styles.totalPosts}>{totalPosts}</p>
+        </div>
+      </div>
+
+      {/*Stats */}
+      <div className={styles.statsContainer}>
+        <p>{reports}</p>
+        <p>{Math.round(hatePercent * 100)}%</p>
+        <p>{Math.round(notPercent * 100)}%</p>
+        <p>{Math.round(prfnPercent * 100)}%</p>
+        <p>{Math.round(racePercent * 100)}%</p>
+        <p>{Math.round(religionPercent * 100)}%</p>
+        <p>{Math.round(genderPercent * 100)}%</p>
+        <Button className={styles.Button} variant="contained">
+          info
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default UserReports;
