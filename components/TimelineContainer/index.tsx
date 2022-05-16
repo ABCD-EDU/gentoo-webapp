470d14190b4237479443235b6630284f42e81f60
arevalolance <lancearevalo2000@gmail.com>
2022-05-16 14:40:06 +0800

    refactor: changed timeline width from 650px to 850px

--- components/TimelineContainer/index.tsx
+++ components/TimelineContainer/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 interface ContainerProps {
   children?: React.ReactNode;
   className?: string;
-  heading: string;
+  heading?: string;
 }
 
 const TimelineContainer: FC<ContainerProps> = ({
@@ -13,11 +13,13 @@ const TimelineContainer: FC<ContainerProps> = ({
 }: ContainerProps) => {
   return (
     <div
-      className={`flex flex-col whitespace-pre-wrap w-[650px] border-[#808080] border-r-[1px] border-l-[1px] ${className}`}
+      className={`flex flex-col whitespace-pre-wrap w-[850px] border-[#808080] border-r-[1px] border-l-[1px] ${className}`}
     >
-      <span className="font-bold font-Inter text-[1.7rem] text-white mt-3 h-fit w-[100%] px-5">
-        {heading}
-      </span>
+      {heading ? (
+        <span className="font-bold font-Inter text-[1.7rem] text-white mt-3 h-fit w-[100%] px-5">
+          {heading}
+        </span>
+      ) : null}
       {children}
     </div>
   );
