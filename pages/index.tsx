2a96f0a6d752b0837564e84d1bb0d744f9a241e8
arevalolance <lancearevalo2000@gmail.com>
2022-05-20 03:42:57 +0800

    add: set default hateFilter value of 100 on access of index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,13 @@ import Logo, { LogoVariants } from "../components/Logo";
 import AboutCard from "../components/AboutCard";
 import SignIn from "../components/SignIn";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    localStorage.setItem("hateFilter", "100");
+  }, []);
+
   return (
     <div className={``}>
       <div className={`flex flex-col w-full justify-center items-center`}>
